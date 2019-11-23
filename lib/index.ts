e6d2a28f2d4b8e26dc70b523d79b2ad6a8484a83
Jaccob Kii <jaccobkii@gmail.com>
2019-11-24 02:38:44 +1100

    bugfix

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,4 @@
 export * from './ZHTClientAPI';
 export * from './ZHTWorkerClientAPI';
+export * from './utils/crypto'
+export * from './utils/net'
---
+++ lib/utils/crypto/index.ts
@@ -0,0 +1,4 @@
+export * from './aes'
+export * from './rsa'
+export * from './base64'
+export * from './sha256'
