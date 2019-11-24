da71902db3525288a57d8936281acca23e897f0c
Jaccob Kii <jaccobkii@gmail.com>
2019-11-25 03:38:24 +1100

    v0.0.14

--- lib/meta/gallery.ts
+++ lib/meta/gallery.ts
@@ -1,5 +1,5 @@
 import { ZHTBaseMeta } from './base';
 export interface GalleryMeta extends ZHTBaseMeta<"gallery"> {
     pageNumber: number
-    files: {[key: number]: number}
+    files: {[key: number]: string}
 }
