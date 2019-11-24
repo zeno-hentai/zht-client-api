b9fc59acd6d70de22d16060d2bacec140f5c7c03
Jaccob Kii <jaccobkii@gmail.com>
2019-11-25 02:58:50 +1100

    add other titles

--- lib/meta/base.ts
+++ lib/meta/base.ts
@@ -1,6 +1,11 @@
+export type ZHTLanguage = "zh-CN" | "en-US" | "jp"
+
 export interface ZHTBaseMeta<Type extends string> {
     type: Type
     title: string
+    subTitles: {
+        [K in ZHTLanguage]: string
+    }
     description: string
-    language: "zh-CN" | "en-US" | "jp"
+    language: ZHTLanguage
 }
