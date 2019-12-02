e6093d6610cd8865e9891054c667dae1c099dab8
Jaccob Kii <jaccobkii@gmail.com>
2019-12-03 05:41:37 +1100

    update gallery meta

--- lib/meta/gallery.ts
+++ lib/meta/gallery.ts
@@ -2,4 +2,5 @@ import { ZHTBaseMeta } from './base';
 export interface GalleryMeta extends ZHTBaseMeta<"gallery"> {
     pageNumber: number
     files: {[key: number]: string}
+    preview: string
 }
